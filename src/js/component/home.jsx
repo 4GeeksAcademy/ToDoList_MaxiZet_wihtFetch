69afd215bcd8b84f009fc6be6e65fd803ebb349f
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-11-03 02:54:14 +0000

    TodoList with Fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,28 +9,54 @@ const Home = () => {
 	
 	async function agregarTareas(evento) {
 		evento.preventDefault()
-		if (tarea != "") {
-			setListaDeTareas([...listaDeTareas, nuevaTarea])
-			setNuevaTarea("")
-		} else {
-			alert ("Ingresar Tarea")
-		}
+		// if (nuevaTarea != "") {
+		// 	guardarTareas()
+		// 	setNuevaTarea("")
+		// } else {
+		// 	alert("Ingresar Tarea")
+		// }
 		if (evento.key === "Enter") {
+			guardarTareas()
+			setNuevaTarea("")
+		}
+	}
+
+	const guardarTareas = async () => {
+		try {
 			const url = "https://playground.4geeks.com/todo/todos/MaxiZet"
-			const resp = await fetch(url,{
-				method:"POST",
-				headers: {
-					'Content-Type': 'application/json'
-				  },
-				  body: JSON.stringify({
+			const resp = await fetch(url, {
+				method: "POST",
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
 					label: nuevaTarea,
 					is_done: false
-				  })
+				})
 			})
 			if (resp.ok) {
 				cargarTareas()
 				return true
 			}
+		} catch (error) {
+			console.log(error)
+			return false
+		}
+	}
+
+
+	const cargarTareas = async () =>{
+		try {
+			const url = "https://playground.4geeks.com/todo/users/MaxiZet"
+			const resp = await fetch(url)
+			if (resp.status == 404) {
+				crearUsuario()
+				return
+			}
+			const data = await resp.json()
+			setListaDeTareas(data.todos)
+			return true
+		} catch (error) {
+			console.error(error)
+			return false
 		}
 	}
 
@@ -48,19 +74,28 @@ const Home = () => {
 			return false
 		}
 	}
-	
-	
-	
-	useEffect(()=> {
-		const cargarTareas = async () => {
-			const url = "https://playground.4geeks.com/todo/users/MaxiZet"
-			const resp = await fetch(url)
-			const data = await resp.json()
-			setListaDeTareas(data.todos) 
+
+	const borrar = async (id) => {
+		try {
+			const resp = await fetch("https://playground.4geeks.com/todo/todos/" + id, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" }
+			})
+			if (resp.status == 204) {
+				cargarTareas()
+				return true
+			}
+		} catch (error) {
+			console.log(error)
+			return false
 		}
+	}
+	
+	useEffect (() => {
 		cargarTareas()
 	},[])
-
+	
+	
 	return (
 		<div className="container mt-5 bg-primary bg-gradient">
 			<h1 className="text-center mt-5 text-light">To do List!</h1>
@@ -68,24 +103,19 @@ const Home = () => {
 				<input type="text" className="form-control" placeholder="¿Que necesitas hacer?" value={nuevaTarea} onChange={(evento) => {
 						setNuevaTarea(evento.target.value)
 					}}
-					onKeyDown={(evento) => {
-						if (evento.key == "Enter") {
-							setListaDeTareas([...listaDeTareas, nuevaTarea]);
-							setNuevaTarea("")
-						}
-					}}
+					 onKeyDown={(evento) => {
+					 	if (evento.key == "Enter") {
+					 		agregarTareas(evento)
+							
+					 	}
+					 }}
 					/>
 				<ul className="card text-light bg-dark bg-gradient">
 
 					{listaDeTareas.map((item,index) => {
 						return (
 							<li key={index}>
-								{item.label} <i onClick={()=> {
-									const aux = listaDeTareas.filter((_task,ind) => {
-										return(ind != index)
-									})
-									setListaDeTareas(aux)
-								}} className="fa-solid fa-trash icono-oculto"></i>
+								{item.label} <i onClick={()=>borrar(item.id)} className="fa-solid fa-trash icono-oculto"></i>
 							</li>
 						)
 					})}
